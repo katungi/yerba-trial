17a19c83514a53befe331aaccb906a136f4ba3f1
katungi <dankatdennis@gmail.com>
2022-04-01 07:34:12 +0300

    - added preview screen

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useCallback } from "react";
 import Editor from "../components/Editor";
+import Preview from "../components/Preview";
 
 export default function Web() {
   const [doc, setDoc] = useState<string>('# Hello, World! now\n')
@@ -11,6 +12,7 @@ export default function Web() {
   return (
     <div className="flex flex-row h-full bg-background">
       <Editor onChange={handleDocChange} initialDoc={doc} />
+      <Preview doc={doc}/>
     </div>
   );
 }
